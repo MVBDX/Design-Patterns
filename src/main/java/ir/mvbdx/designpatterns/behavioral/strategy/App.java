42383e84479a98f342f6340b69bad0613adb5e2c
MVBDX <faalian.m@gmail.com>
2022-11-17 10:05:24 +0330

    feat strategy hashmap to remove if-else conditionals

--- src/main/java/ir/mvbdx/designpatterns/behavioral/strategy/App.java
+++ src/main/java/ir/mvbdx/designpatterns/behavioral/strategy/App.java
@@ -1,21 +1,32 @@
 package ir.mvbdx.designpatterns.behavioral.strategy;
 
-import ir.mvbdx.designpatterns.behavioral.strategy.calculation.Addition;
-import ir.mvbdx.designpatterns.behavioral.strategy.calculation.Calculator;
-import ir.mvbdx.designpatterns.behavioral.strategy.calculation.Multiply;
+import ir.mvbdx.designpatterns.behavioral.strategy.calculation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        double num1 = 2.3d;
-        double num2 = 5.4d;
+        Scanner scanner = new Scanner(System.in);
+        Map<String, CalculateStrategy> strategyMap = new HashMap<>();
+        strategyMap.put("add", new Addition());
+        strategyMap.put("div", new Division());
+        strategyMap.put("mul", new Multiply());
+        strategyMap.put("sub", new Subtraction());
 
         Calculator calculator = new Calculator();
 
-        calculator.setStrategy(new Multiply());
-        System.out.printf("Multiply of %f and %f is %f%n", num1, num2, calculator.calculate(num1, num2));
+        System.out.println("Please choose your operation: add / div / mul / sub");
+        String operation = scanner.nextLine();
+        System.out.println("Please enter your first number:");
+        double num1 = scanner.nextDouble();
+        System.out.println("Please enter your second number:");
+        double num2 = scanner.nextDouble();
 
-        calculator.setStrategy(new Addition());
-        System.out.printf("Addition of %f and %f is %f%n", num1, num2, calculator.calculate(num1, num2));
+        calculator.setStrategy(strategyMap.get(operation));
+
+        System.out.printf("%s of %f and %f is %f%n", operation, num1, num2, calculator.calculate(num1, num2));
 
     }
 }
