8cbaff9c3e6c44a37ffe067a89971dd5123a6703
MVBDX <faalian.m@gmail.com>
2022-11-17 12:29:03 +0330

    feat builder: to handle telescoping constructors problem

---
+++ src/main/java/ir/mvbdx/designpatterns/creational/builder/Demo.java
@@ -0,0 +1,14 @@
+package ir.mvbdx.designpatterns.creational.builder;
+
+public class Demo {
+    public static void main(String[] args) {
+        User user = new User.UserBuilder("Yashar", "Faalian")
+                .setAge(34)
+                .setPhone("0936123456")
+                .setAddress("Fake address 1234")
+                .build();
+
+        System.out.println(user);
+
+    }
+}
